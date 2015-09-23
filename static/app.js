13c0d18b9c18f14ffabf745c5c420f466a305a9f
Ramon Saraiva <ramonsaraiva@gmail.com>
2015-09-23 04:00:38 -0300

    added some base page to create a bingo match

--- static/app.js
+++ static/app.js
@@ -3,13 +3,13 @@
 var app = angular.module('app', [
 	'ngRoute',
 	'controllers',
-	'services',
+	'services'
 ]);
 
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/bingo/', {
-			templateUrl: 'partials/bingo.tpl.html',
+			templateUrl: 'partials/bingo.tpl.html'
 		})
 		.otherwise({
 			redirectTo: '/bingo/'
--- static/controllers.js
+++ static/controllers.js
@@ -3,5 +3,17 @@
 var controllers = angular.module('controllers', []);
 
 controllers.controller('bingo_controller', ['$scope', '$location', '$http', function($scope, $location, $http) {
-	$scope.ramon = 'oi';
+	$scope.players = 4;
+	$scope.match = null;
+
+	$scope.create = function()
+	{
+		$http.post('/matches/', {'players': $scope.players})
+			.success(function(data) {
+				$scope.match = data;
+			})
+			.error(function(e) {
+				alert('error');
+			});
+	}
 }]);
